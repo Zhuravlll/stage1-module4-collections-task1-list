89aa4fb426013b2862ba6c79e8fc642c09844231
Ivan_Zhuravlov <vanya7zhuravlov@gmail.com>
2023-06-12 15:30:12 +0300

    Stage 1, module 4, completed tasks with lists.

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,13 +1,12 @@
 package com.epam.mjc.collections.list;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
         ListComparator listComparator = new ListComparator();
-        Collections.sort(sourceList, listComparator);
+        sourceList.sort(listComparator);
     }
 }
 
