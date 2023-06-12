a00dbf351f3cde47b934c6d5091fa67c1555b579
Ivan_Zhuravlov <vanya7zhuravlov@gmail.com>
2023-06-12 15:29:22 +0300

    Stage 1, module 4, completed tasks with lists.

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,15 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (Integer e : sourceList) {
+            if (e % 2 == 0) {
+                list.addLast(e);
+            }
+            else {
+                list.addFirst(e);
+            }
+        }
+        return list;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,31 @@
 package com.epam.mjc.collections.list;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        ListComparator listComparator = new ListComparator();
+        Collections.sort(sourceList, listComparator);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        double firstDigit = Double.parseDouble(a);
+        double secondDigit = Double.parseDouble(b);
+        double firstCompute = Math.pow(firstDigit, 2) * 5 + 3;
+        double secondCompute = Math.pow(secondDigit, 2) * 5 + 3;
+        if (firstCompute == secondCompute && firstDigit > secondDigit) {
+            return 1;
+        }
+        if (firstCompute > secondCompute) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
     }
 }
